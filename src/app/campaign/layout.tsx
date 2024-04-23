3e53d774f08206ba8e23f537ec9b16b126adb3c6
Daybreak <daybreak@chtrak.com>
2024-04-23 16:03:46 +0800

    set all page title

--- src/app/campaign/layout.tsx
+++ src/app/campaign/layout.tsx
@@ -2,6 +2,6 @@ import { Metadata } from "next";
 import Page from "./page";   // import your Demo's page
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
+    title: 'Compaign Hub | Cusob',
 };
 export default Page;
--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -2,6 +2,6 @@ import { Metadata } from "next";
 import Page from "./page";   // import your Demo's page
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
+    title: 'Signup | Cusob',
 };
 export default Page;
