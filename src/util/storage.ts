75ff60e73287d5e90f3ac41a79e4fa89417ed695
xtl <941563132@qq.com>
2024-04-25 09:33:34 +0800

    cookie_1

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -7,36 +7,11 @@ export type LocalUser = {
 }
 
 export const getToken = () => {
-
-
-  const itemStr = localStorage.getItem('token');
-  if (!itemStr) {
-    return ''; // 如果数据不存在，直接返回 null
-  }
-  const item = JSON.parse(itemStr);
-  const now = new Date();
-  if (now.getTime() > item.expiry) {
-    localStorage.removeItem('token'); // 如果数据已过期，则删除数据并返回 null
-    return '';
-  }
-  return item.value; // 如果数据未过期，则返回数据值
+  return window.localStorage.getItem('token'); // 如果数据未过期，则返回数据值
 };
 
-export const setAcceptFalse=(isAccept: string)=>{
-  window.localStorage.setItem("Accept",'false')
-}
-
-export const getAccept=()=>{
-  return  window.localStorage.getItem("Accept")
-}
-
 export const setToken = (token: string) => {
-  const now = new Date();
-  const item = {
-    value: token || '',
-    expiry: now.getTime() + 3600 * 1000*2 // 过期时间为当前时间加上 2h
-  };
-  window.localStorage.setItem('token', JSON.stringify(item))
+  window.localStorage.setItem('token', token)
 }
 
 export const clearToken = () => {
