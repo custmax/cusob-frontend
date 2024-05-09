6f7cff23db0cc6db99aa860b03c26bbcdb4d9a12
Daybreak <daybreak@chtrak.com>
2024-05-09 14:17:14 +0800

    add,update,delete,select template

--- src/app/sendcloud/page.tsx
+++ src/app/sendcloud/page.tsx
@@ -8,6 +8,7 @@ import {Button} from "antd";
 import {SUCCESS_CODE} from "@/constant/common";
 import {getDomainList, addDomain, updateDomain, checkDomain, deleteDomain} from "@/server/sendcloud/domain";
 import {getApiUserList, addApiUser} from "@/server/sendcloud/apiUser";
+import {getTemplateList, getTemplateDetail, addTemplate, removeTemplate, updateTemplate} from "@/server/sendcloud/template";
 
 const {
     sendCloudContainer,
@@ -61,11 +62,80 @@ const SendCloud = () => {
         const res = await addApiUser('cusob_batch02', 'mail.dlgems.com')
     }
 
+    const findTemplateList = async () => {
+        const res = await getTemplateList()
+    }
+
+    const getTemplate = async () => {
+        const res = await getTemplateDetail('template01')
+    }
+
+    const saveTemplate = async () => {
+        const html = "<table class=\"inner_table\" align=\"center\" width=\"600\">\n" +
+            "    <tbody>\n" +
+            "        <tr class=\"firstRow\">\n" +
+            "            <td align=\"center\" style=\"font-family: Geist, sans-serif; font-size: 38px; -webkit-font-smoothing: subpixel-antialiased; text-size-adjust: 100%; line-height: 46px; text-align: center; font-weight: 600; letter-spacing: -0.04em;\">\n" +
+            "                <p style=\"line-height: 64.6px; text-size-adjust: 100%;\">\n" +
+            "                    <strong>Vercel Product Update</strong>\n" +
+            "                </p>\n" +
+            "            </td>\n" +
+            "        </tr>\n" +
+            "        <tr>\n" +
+            "            <td style=\"font-size: 1px; -webkit-font-smoothing: subpixel-antialiased; text-size-adjust: 100%; line-height: 1px;\"></td>\n" +
+            "        </tr>\n" +
+            "        <tr>\n" +
+            "            <td align=\"center\" style=\"font-family: Geist, sans-serif; font-size: 16px; -webkit-font-smoothing: subpixel-antialiased; text-size-adjust: 100%; color: rgb(68, 68, 68); line-height: 24px; text-align: center;\">\n" +
+            "                <p>\n" +
+            "                    Improved infrastructure pricing | HIPAA compliance | ModelFusion acquisition | Vercel Ship 2024\n" +
+            "                </p>\n" +
+            "            </td>\n" +
+            "        </tr>\n" +
+            "    </tbody>\n" +
+            "</table>\n" +
+            "<p>\n" +
+            "    <br/>\n" +
+            "</p>"
+        const res = await addTemplate('template02', 'template02', html, "Product Update")
+    }
+
+    const deleteTemplate = async () => {
+        const res = await removeTemplate('template02')
+    }
+
+    const editTemplate = async () => {
+        const html = "<table class=\"inner_table\" align=\"center\" width=\"600\">\n" +
+            "    <tbody>\n" +
+            "        <tr class=\"firstRow\">\n" +
+            "            <td align=\"center\" style=\"font-family: Geist, sans-serif; font-size: 38px; -webkit-font-smoothing: subpixel-antialiased; text-size-adjust: 100%; line-height: 46px; text-align: center; font-weight: 600; letter-spacing: -0.04em;\">\n" +
+            "                <p style=\"line-height: 64.6px; text-size-adjust: 100%;\">\n" +
+            "                    <strong>Vercel Product Update</strong>\n" +
+            "                </p>\n" +
+            "            </td>\n" +
+            "        </tr>\n" +
+            "        <tr>\n" +
+            "            <td style=\"font-size: 1px; -webkit-font-smoothing: subpixel-antialiased; text-size-adjust: 100%; line-height: 1px;\"></td>\n" +
+            "        </tr>\n" +
+            "        <tr>\n" +
+            "            <td align=\"center\" style=\"font-family: Geist, sans-serif; font-size: 16px; -webkit-font-smoothing: subpixel-antialiased; text-size-adjust: 100%; color: rgb(68, 68, 68); line-height: 24px; text-align: center;\">\n" +
+            "                <p>\n" +
+            "                    Improved infrastructure pricing | HIPAA compliance | ModelFusion acquisition | Vercel Ship 2024\n" +
+            "                </p>\n" +
+            "            </td>\n" +
+            "        </tr>\n" +
+            "    </tbody>\n" +
+            "</table>\n" +
+            "<p>\n" +
+            "    <br/>\n" +
+            "</p>"
+        const res = await updateTemplate('template01', 'template01', html)
+    }
+
+
     return <div className={sendCloudContainer}>
         <EnteredHeader />
         <SideBar />
         <div className={main}>
-            <Button className={btnSend} onClick={saveApiUser}>Send</Button>
+            <Button className={btnSend} onClick={editTemplate}>Send</Button>
 
         </div>
     </div>
