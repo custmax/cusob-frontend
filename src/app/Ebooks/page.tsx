74599fb44796a1e148e633162182cb2c424a2cba
luozhiyuan <2059374416@qq.com>
2024-10-08 17:01:50 +0800

    2024.10.08 v2 ebooks前端

--- src/app/Ebooks/page.tsx
+++ src/app/Ebooks/page.tsx
@@ -1,83 +1,110 @@
-// @ts-ignore
+// pages/Ebooks/EbookPage.tsx
 "use client";
-import React, { useState } from 'react'; // 不推荐使用 @ts-ignore
-
+import React, { useState } from 'react';
+import Link from 'next/link';
+import UserInfo from './userInfo/page'; // 导入 UserInfo 组件
+import MyBooks from './MyBooks/page';
+import MyAccount from '@/app/Ebooks/myaccount/page'// 导入 MyBooks 组件
 
 const EbookPage: React.FC = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const [cart, setCart] = useState<number[]>([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true); // 用户登录状态
+    const [activeTab, setActiveTab] = useState<'userinfo' | 'mybooks' | 'myaccount' | 'mytrade' | 'myupload'>('userinfo');
 
-    // 电子书示例数据
-    const ebooks = [
-        { id: 1, title: 'React for Beginners', author: 'John Doe', price: 10 },
-        { id: 2, title: 'Advanced Tailwind CSS', author: 'Jane Smith', price: 15 },
-        { id: 3, title: 'Understanding Shadcn', author: 'Alice Johnson', price: 12 },
-    ];
-
-    const handleLogin = () => {
-        setIsLoggedIn(true);
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
     };
 
-    const addToCart = (id: number) => {
-        setCart([...cart, id]);
+    const handleLogin = () => {
+        // 跳转到登录页面
+        window.location.href = '/Ebooks/Login'; // 跳转到登录页面
+    };
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
     };
 
     return (
         <div className="bg-gray-100 min-h-screen flex flex-col items-center p-8">
-            <header className="w-full mb-8">
-                <h1 className="text-3xl font-bold text-center">电子书商店</h1>
+            <header className="w-3/4 mb-8 flex justify-between items-center">
+                <h1 className="text-3xl font-bold">电子书商店</h1>
+                <div className="relative">
+                    <button
+                        className="bg-blue-600 text-white py-2 px-4 rounded"
+                        onClick={isLoggedIn ? toggleDropdown : handleLogin}
+                    >
+                        {isLoggedIn ? '我的账户' : '登录'}
+                    </button>
+                    {showDropdown && isLoggedIn && (
+                        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg">
+                            <ul className="py-2">
+                                    <button
+                                        className="block px-4 py-2 text-gray-800 hover:bg-gray-200 w-full text-left"
+                                        onClick={handleLogout}
+                                    >
+                                        登出
+                                    </button>
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </header>
-            {/* 登录模块 */}
-            <div className="bg-white shadow-md rounded-lg p-6 mb-8 w-full max-w-md">
-                {!isLoggedIn ? (
-                    <div>
-                        <h2 className="text-xl font-semibold mb-4">登录</h2>
+
+            {/* 选项卡导航 */}
+            <div className="hidden sm:block w-full mb-4">
+                <nav className="-mb-px flex w-full justify-center " aria-label="Tabs">
+                    <div className="border-b w-3/4 flex border-gray-200 gap-6">
                         <button
-                            className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition"
-                            onClick={handleLogin}
+                            onClick={() => setActiveTab('userinfo')}
+                            className={`shrink-0 border-b-2 px-1 pb-4 text-sm font-medium ${
+                                activeTab === 'userinfo' ? 'border-sky-500 text-sky-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                            }`}
                         >
-                            登录
+                            我的信息
                         </button>
-                    </div>
-                ) : (
-                    <p className="text-green-600">欢迎回来！</p>
-                )}
-            </div>
-            {/* 购物车模块 */}
-            <div className="bg-white shadow-md rounded-lg p-6 mb-8 w-full max-w-md">
-                <h2 className="text-xl font-semibold mb-4">购物车 ({cart.length} 件商品)</h2>
-                {cart.length === 0 ? (
-                    <p>购物车为空</p>
-                ) : (
-                    <ul>
-                        {cart.map((id) => {
-                            const ebook = ebooks.find((ebook) => ebook.id === id);
-                            return (
-                                <li key={id} className="flex justify-between mb-2">
-                                    <span>{ebook?.title}</span>
-                                    <span>${ebook?.price}</span>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                )}
-            </div>
-            {/* 电子书展示模块 */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-                {ebooks.map((ebook) => (
-                    <div key={ebook.id} className="bg-white shadow-md rounded-lg p-4">
-                        <h3 className="text-lg font-semibold">{ebook.title}</h3>
-                        <p className="text-gray-600">作者: {ebook.author}</p>
-                        <p className="text-xl font-bold">${ebook.price}</p>
                         <button
-                            className="mt-4 bg-green-600 text-white py-2 px-4 rounded hover:bg-green-700 transition"
-                            onClick={() => addToCart(ebook.id)}
+                            onClick={() => setActiveTab('mybooks')}
+                            className={`shrink-0 border-b-2 px-1 pb-4 text-sm font-medium ${
+                                activeTab === 'mybooks' ? 'border-sky-500 text-sky-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                            }`}
                         >
-                            添加到购物车
+                            我的图书
                         </button>
+                        <button
+                            onClick={() => setActiveTab('myaccount')}
+                            className={`shrink-0 border-b-2 px-1 pb-4 text-sm font-medium ${
+                                activeTab === 'myaccount' ? 'border-sky-500 text-sky-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                            }`}
+                        >
+                            我的账户
+                        </button>
+                        <button
+                            onClick={() => setActiveTab('myaccount')}
+                            className={`shrink-0 border-b-2 px-1 pb-4 text-sm font-medium ${
+                                activeTab === 'mytrade' ? 'border-sky-500 text-sky-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                            }`}
+                        >
+                            我的交易
+                        </button>
+
+                        <button
+                            onClick={() => setActiveTab('myaccount')}
+                            className={`shrink-0 border-b-2 px-1 pb-4 text-sm font-medium ${
+                                activeTab === 'myupload' ? 'border-sky-500 text-sky-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'
+                            }`}
+                        >
+                            我的上传
+                        </button>
+
                     </div>
-                ))}
+                </nav>
+
             </div>
+
+            {/* 根据选中的选项卡渲染内容 */}
+            {activeTab === 'userinfo' && <UserInfo/>}
+            {activeTab === 'mybooks' && <MyBooks/>}
+            {activeTab === 'myaccount' && <MyAccount/>}
         </div>
     );
 };
