0581ec3c55bc564a86c68347090358a4c0d172fb
马俊男 <2506774772@qq.com>
2024-12-31 15:06:08 +0800

    添加了BookDemo

--- src/app/custmax/bookDemoSuccess/page.tsx
+++ src/app/custmax/bookDemoSuccess/page.tsx
@@ -1,33 +1,14 @@
-import LighterFooter from "@/app/custmax/component/LighterFooter";
 import React from "react";
-
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import Footer from "@/app/custmax/component/Footer";
+import Header from "@/app/custmax/component/Header";
+import BookDemoSuccessPage from "@/app/custmax/component/BookDemoSuccessPage";
 
 export default function BookDemoSuccess() {
     return (
-        <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32">
-            <div className="mx-auto max-w-3xl text-center mb-16 pt-0">
-                <h2 className="text-4xl font-bold tracking-tight sm:text-4xl mb-6">
-                    Thank you for reaching out !
-                    <p className="text-lg leading-8 text-gray-600">
-                        Your request has been received successfully.
-                    </p>
-                </h2>
-
-                <p className="text-lg leading-8 text-gray-600 max-w-2xl mx-auto px-4 py-6">
-                    Our team will reach out soon to confirm the details and guide you through how CustMax can
-                    supercharge your email marketing.
-                </p>
-                <h2 className="text-4xl font-bold tracking-tight sm:text-4xl mb-6">
-                    <p className="text-lg leading-8 text-gray-600">
-                        We’re excited to connect with you! 🚀
-                    </p>
-                </h2>
-
-            </div>
-
-            <LighterFooter />
+        <div>
+            <Header />
+            <BookDemoSuccessPage />
+            <Footer theme={"light"} />
         </div>
-
     )
 }
